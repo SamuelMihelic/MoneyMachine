6540d23a591cce691f74f8e57d9740169b1675b0
Samuel A. Mihelic <samuelmihelic@gmail.com>
2022-07-17 01:37:37 -0500

    Update MoneyMachine_library.py

--- main/MoneyMachine_library.py
+++ main/MoneyMachine_library.py
@@ -1,6 +1,31 @@
+class model:
+  def __init__( self, type, time_constant )
+      self.tau  = time_constant
+      self.type = type
+    
+  def fitting( self, data )
+    # time-weighted windowing with a half-Gaussian
+    weighted_data_values = data.values 
+  
+    if self.type is 'zero_order'
+    elseif self.type is 'first_order'
+    elseif self.type is 'second_order'
+    else
+      # throw error
+      
+class data:
+  def __init__( self, values, times )
+    self.values = values
+    self._times =  times
+
+  def update( self, value, time )
+    self.values.append(value)
+    self._times.append( time)
+
 class PID:
   def __init__( self, PID_constants ):
-    self.constants = PID_constants
+    self.constants =  PID_constants
+    
     self.P_error = 0
     self.I_error = 0
     self.D_error = 0
@@ -9,6 +34,7 @@ class PID:
     response = self.P * error.P \
              + self.I * error.I \
              + self.D * error.D
+             
     return response
   
 class error:
