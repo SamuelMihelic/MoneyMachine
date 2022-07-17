158d1ebc95562c97fe76600a18695d4d18fac5da
Samuel A. Mihelic <samuelmihelic@gmail.com>
2022-07-17 00:44:27 -0500

    Update and rename MoneyMachine_library to MoneyMachine_library.py

--- main/MoneyMachine_library.py
+++ main/MoneyMachine_library.py
@@ -1,14 +1,18 @@
 class PID:
-  def __init__(self, PID_constants ):
+  def __init__( self, PID_constants ):
     self.constants = PID_constants
     self.P_error = 0
     self.I_error = 0
     self.D_error = 0
     
-  def response( self, error )
+  def response( self, error ):
+    response = self.P * error.P \
+               self.I * error.I \
+               self.D * error.D
+    return response
   
-class error
-  def __init__(self):
+class error:
+  def __init__( self ):
     self.P = 0
     self.I = 0
     self.D = 0
