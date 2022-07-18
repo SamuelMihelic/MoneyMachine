151c43aaf79f813864fca1e3a8096be5c576a453
Samuel A. Mihelic <samuelmihelic@gmail.com>
2022-07-18 05:10:51 -0500

    Update MoneyMachine.py

--- MoneyMachine.py
+++ MoneyMachine.py
@@ -3,7 +3,8 @@
 
             # According to [the efficient market hypothesis](https://en.wikipedia.org/wiki/Efficient-market_hypothesis) (as well as results from probability theory on [submartingales](https://en.m.wikipedia.org/wiki/Martingale_(probability_theory))), this algorithm will not outperform a simple holding strategy... But we will put this to the test, and at the very least, this algorithm will provide price stability (denoising, not trend-shifting) if enough people adopt it, through the use of [PID control](https://en.wikipedia.org/wiki/PID_controller).
             # SAM 2/19/22
-
+import MoneyMachine_library as mm
+            
 class exe:
             def __init__( self, exchange, account_credentials, asset, bsset, baseline_proportion, PID_constants, Gaussian_window_length, model_order )
                         
